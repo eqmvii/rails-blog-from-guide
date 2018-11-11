15681d23149f8c1a568c7f949d97987be1a74c46
Eric (Martin) Mancini <coelomate@gmail.com>
2018-11-11 15:52:00 -0500

    more error checking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new # Needed to prevent the view error check from throing an error (@article.errors.any? )
   end
 
   def create
