72cfbb6d95c3ca5daed86c8e0643fcb7e1e3c1f7
Eric (Martin) Mancini <coelomate@gmail.com>
2018-11-17 10:57:54 -0500

    commenting working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    # Recommended order: index, show, new, edit, create, update and destroy
+
+    def create
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.create(comment_params) # Automatically links the new comment to the original article
+
+      redirect_to article_path(@article)
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
